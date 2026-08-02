02970f181730947de2f2347898dcb360185a73f6
Shrey-2828 <ldce.2324@gmail.com>
2026-08-03 00:08:59 +0530

    hyperparameter tuning done with mlflow

--- src/file3.py
+++ src/file3.py
@@ -16,8 +16,8 @@ x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42
 
 #params
 grid={
-    'max_depth': [None,3,5,7],
-    'n_estimators':[10,50,100]
+    'max_depth': [None,3,5,7,9],
+    'n_estimators':[10,50,100,150]
 }
 
 rf=RandomForestClassifier()
@@ -28,28 +28,22 @@ dagshub.init(repo_owner='Shrey-2828', repo_name='MLOPS-MLflow', mlflow=True)
 
 #mlflow
 mlflow.set_tracking_uri('https://dagshub.com/Shrey-2828/MLOPS-MLflow.mlflow')
-mlflow.set_experiment("Hyper parameter tuning")
+mlflow.set_experiment("Breast cancer detection")
 
-with mlflow.start_run():
+with mlflow.start_run() as parent:
     gd.fit(x_train,y_train)
+    
+    for i in range (len(gd.cv_results_['params'])):
+        
+        with mlflow.start_run(nested=True) as child: 
+         mlflow.log_params(gd.cv_results_['params'][i])
+         mlflow.log_metric("accuracy",gd.cv_results_["mean_test_score"][i])
     
     best_param=gd.best_params_
     best_score=gd.best_score_
     
-    train_df=x_train.copy()
-    train_df['target']=y_train
-    
-    train_df=mlflow.data.from_pandas(train_df)
-    mlflow.log_input(train_df,"training")
-    
-    test_df=x_test.copy()
-    test_df['target']=y_test
-    
-    test_df=mlflow.data.from_pandas(test_df)
-    mlflow.log_input(test_df,"testing")
     
     mlflow.set_tag('author','shrey patel')
-    
     mlflow.log_artifact(__file__)
     mlflow.log_metric("accuracy",best_score)
     mlflow.sklearn.log_model(gd.best_estimator_ , 'Random forest')
